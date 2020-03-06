bd05226875c463a41e776e984c8fdf91455c46e4
chenlingxi@ <chanlingxi@gmail.com>
2020-03-06 22:27:56 +0800

    add bed2matrix.py

---
+++ script/bed2matrix.py
@@ -0,0 +1,81 @@
+#!/usr/bin/env python
+# -*- coding: utf-8 -*-
+"""
+# transfer bed file to matrix csv file
+
+Usage:
+    bed2matrix.py call --bed_fn=IN_FILE [--out_prefix=STR] [--confidence=INT]
+    bed2matrix.py -h | --help
+
+Options:
+    -h --help           Show this screen.
+    --version           Show version.
+    --bed_fn=IN_FILE    Path of bed file.
+    --out_prefix=STR    Path of out file prefix, prefix of bed_fn as default.
+    --confidence=INT    CNV event confidence, only availble for cellranger bed file, [default: 150]
+"""
+import csv
+import sys
+import numpy as np
+import pandas as pd
+import docopt
+
+
+def run_call(bed_fn=None, out_prefix=None, confidence=10,
+    **args):
+
+    breakpoints = {} 
+    cells = set() 
+    bed_file = filter(lambda row: row.startswith('#chrom') or not row.startswith('#'), open(bed_fn, 'r'))
+    for i, row in enumerate(csv.DictReader(bed_file, delimiter='\t')):
+        chrom, start, end, cell_id, cn = row['#chrom'], row['start'], row['end'], row['id'], row['copy_number']
+        cells.add(cell_id)
+        if chrom not in breakpoints:
+            breakpoints[chrom] = set()
+        breakpoints[chrom].add(start)
+        breakpoints[chrom].add(end)
+    cell2index = {cell_id: i for i, cell_id in enumerate(cells)}
+
+    regions = []
+    bp2index = {}
+    for chrom, bps in breakpoints.items():
+        bps = sorted(list(bps))
+        res = {'{}:{}'.format(chrom, start) : len(regions) + i for i, start in enumerate(bps)}
+        bp2index.update(res)
+        regions += ['{}:{}-{}'.format(chrom, start, end) for start, end in zip(bps[:-1], bps[1:])]
+
+    print('cell', len(cells), 'regions', len(regions), 'bp2index', len(bp2index))
+    matrix = np.full([len(cells), len(regions)], np.nan)
+    print(matrix.shape)
+    bed_file = filter(lambda row: row.startswith('#chrom') or not row.startswith('#'), open(bed_fn, 'r'))
+    for i, row in enumerate(csv.DictReader(bed_file, delimiter='\t')):
+        if row.get('event_confidence', confidence) < confidence:
+            continue
+        chrom, start, end, cell_id, cn = row['#chrom'], row['start'], row['end'], row['id'], row['copy_number']
+        start_index = bp2index['{}:{}'.format(chrom, start)] 
+        end_index = bp2index['{}:{}'.format(chrom, end)]
+        for i in range(start_index, end_index):
+            matrix[cell2index[cell_id], i] = cn
+
+    print('start writing into result.cnv.csv')
+    pd.set_option('display.max_columns', None)
+    pd.set_option('display.max_rows', None)
+    x = pd.DataFrame(matrix, index = cell2index.keys(), columns=regions)
+    if not out_prefix:
+        _, out_prefix = os.split(bed_fn)
+    x.to_csv(out_prefix + '.cnv.csv', index=True)
+    print('finished')
+
+
+def run(call=None, **args):
+    if call:
+        run_call(**args)
+
+
+if __name__ == "__main__":
+    args = docopt.docopt(__doc__)
+    new_args = {}
+    for k, v in args.items():
+        new_args[k.replace('--', '')] = v
+    run(**new_args)
+
