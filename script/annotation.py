c0c1435e7c7922368063f2bf38184d4fb7c83f7c
chenlingxi@dl380a <chanlingxi@gmail.com>
2020-03-11 15:39:00 +0800

    annotation.py remove na in data_df

--- script/annotation.py
+++ script/annotation.py
@@ -32,14 +32,17 @@ def get_cnv_bed(cnv_fn):
 
 def get_gene_bed(ref):
     gene_dict = pickle.load(open('db/ens_gene.pickle', 'rb')).get(ref, {})
-    for gene, region in gene_dict.items():
+    bed_str = ''
+    for i, item in enumerate(gene_dict.items()):
+        if i > 200: break
+        gene, region = item
         chrom, start, end, ens_id = region
-        bed_str = '{}\t{}\t{}\t{}\n'.format(chrom, start, end, ens_id)
-        yield gene, BedTool(bed_str, from_string=True)
+        bed_str += '{}\t{}\t{}\t{}\n'.format(chrom, start, end, gene+','+ens_id)
+    return BedTool(bed_str, from_string=True)
 
 
 def process_single_hit(gene, hit, cnv_df, feature_list, data_df, matrix_df):
-    ens_id = hit[3]
+    gene, ens_id = hit[3].split(',')
     feature_list.append([ens_id, gene, 'CNV Profile'])
 
     cnv_bed_str = '{}:{}-{}'.format(hit[4], hit[5], hit[6])
@@ -53,11 +56,11 @@ def process_single_hit(gene, hit, cnv_df, feature_list, data_df, matrix_df):
     return data_df
 
 
-def process_multi_hits(gene, hits, cnv_df, feature_list, data_df, matrix_df):
+def process_hits(gene, hits, cnv_df, feature_list, data_df, matrix_df):
 
     cnv_bed_str_list = []
     for i, hit in enumerate(hits):
-        ens_id = hit[3]
+        gene, ens_id = hit[3].split(',')
         cnv_bed_str_list.append('{}:{}-{}'.format(hit[4], hit[5], hit[6]))
     feature_list.append([ens_id, gene, 'CNV Profile'])
 
@@ -73,32 +76,32 @@ def process_multi_hits(gene, hits, cnv_df, feature_list, data_df, matrix_df):
 
 def run_call(cnv_fn=None, target_gene_fn=None, ref=None, out_prefix=None, **args):
     cnv_df, cnv_bed = get_cnv_bed(cnv_fn)
-
     cnv_df.reset_index(inplace=True)
     cnv_df.index = cnv_df.index + 1
+
+    gene_bed = get_gene_bed(ref)
+
     feature_list = []
     data_df = pd.DataFrame()
     matrix_df = pd.DataFrame()
 
-    for i, item in enumerate(get_gene_bed(ref)):
-        gene, gene_bed = item
-
-        hits = gene_bed.window(cnv_bed).overlap(cols=[2, 3, 6, 7])
-
-        if not hits:
-            continue
-        if len(hits) == 1:
-            continue
-            data_df = process_single_hit(gene, hits[0], cnv_df, feature_list, data_df, matrix_df)
-        else:
-            data_df = process_multi_hits(gene, hits, cnv_df, feature_list, data_df, matrix_df)
+    prev_gene = None
+    hits = []
+    for i, hit in enumerate(gene_bed.window(cnv_bed).overlap(cols=[2, 3, 6, 7])):
+        gene, _ = hit[3].split(',')
+        if prev_gene != gene and hits:
+            data_df = process_hits(gene, hits, cnv_df, feature_list, data_df, matrix_df)
+            hits = []
+        hits.append(hit)
+        prev_gene = gene
 
     # output
-    cnv_df['cell_id'].to_csv('barcodes.tsv.gz', sep='\t', header=False, index=False, compression='gzip')
+    cnv_df['cell_id'].to_csv(out_prefix + '_barcodes.tsv.gz', sep='\t', header=False, index=False, compression='gzip')
 
     feature_df = pd.DataFrame(feature_list)
     feature_df.to_csv(out_prefix + '_features.tsv.gz', sep='\t', header=False, index=False, compression='gzip')
 
+    data_df = data_df.dropna()
     df = pd.DataFrame(data={'0': [matrix_df.shape[1]],
                             '1': [matrix_df.shape[0]],
                             '2': [data_df.shape[0]]})
@@ -108,10 +111,10 @@ def run_call(cnv_fn=None, target_gene_fn=None, ref=None, out_prefix=None, **args
         f.write(b'%% MatrixMarket matrix coordinate integer general\n')
         f.write(b'%% feature index, cell index, copy number profile (index starts with 1)\n')
         f.write(b'%% produced by scVar.1.0 \n')
-    data_df.to_csv(out_prefix + 'matrix.mtx.gz', sep=' ', header=False, index=False, compression='gzip', mode='a')
+    data_df.to_csv(out_prefix + '_matrix.mtx.gz', sep=' ', header=False, index=False, compression='gzip', mode='a')
 
     matrix_df.index = cnv_df['cell_id']
-    matrix_df.to_csv(out_prefix + 'gene_cnv.csv.gz', compression='gzip')
+    matrix_df.to_csv(out_prefix + '_gene_cnv.csv.gz', compression='gzip')
 
 
 def run(call=None, **args):
