e5cc64525368fbc559a2ea56bf8915c2388cb8db
chenlingxi@ <chanlingxi@gmail.com>
2020-03-16 16:30:03 +0800

    reconstruct code & add evo_json

--- script/scVar.py
+++ script/scVar.py
@@ -3,7 +3,7 @@
 """
 
 Usage:
-    scVar.py call --cnv_fn=IN_FILE [--meta_fn=IN_FILE] [-k=INT] [--out_prefix=STR]
+    scVar.py cnv --cnv_fn=IN_FILE [--meta_fn=IN_FILE] [--k=INT] [--out_prefix=STR]
     scVar.py -h | --help
 
 Options:
@@ -11,7 +11,7 @@ Options:
     --version           Show version.
     --cnv_fn=IN_FILE    Path of SCYN format cnv file.
     --meta_fn=IN_FILE   Path of SCYN format meta file.
-    -k=INT              Number of clusters in the tree at the cut point. [default: 3]
+    --k=INT              Number of clusters in the tree at the cut point. [default: 3]
     --out_prefix=STR    Path of out file prefix, [default: ./phylo] 
 """
 import os
@@ -19,30 +19,78 @@ import docopt
 from scipy.cluster import hierarchy
 import numpy as np
 import pandas as pd
+import json
 
-import phylogenetic
-import embedding 
+from utils import phylogenetic as phylo
+from utils import io
+from utils import embedding
 
 
-def read_cnv_fn(cnv_fn):
-    df = pd.read_csv(cnv_fn)
-    cell_names = df.values[:, 0]
-    cnv_m = df.values[:, 1:]
-    return cnv_m, cell_names
-
-def read_meta_fn(meta_fn):
-    df = pd.read_csv(meta_fn, index_col='cell_id')
-    return df
-    
-
-def run_call(cnv_fn=None, meta_fn=None, k=3, cut_n=50,
+def run_cnv(cnv_fn=None, meta_fn=None, k=7, cut_n=25,
              out_prefix=None, **args):
-    cnv_m, cell_names = read_cnv_fn(cnv_fn) 
+    k = int(k)
+    cnv_index_name = 'cell_id'
+    cnv_df = io.read_cnv_fn(cnv_fn, cnv_index_name) 
+    cell_names = cnv_df.index.to_list()
 
+    ##### build hc dendrogram ##### 
+    newick = phylo.build_hc_tree(cnv_df, cnv_index_name)
+    nwk_fn = out_prefix + '.nwk'
+    with open(nwk_fn, 'w') as f:
+        f.write(newick)
+    t = phylo.get_tree_from_newick(newick)
 
-def run(call=None, **args):
-    if call:
-        run_call(**args)
+    ##### cut hc dendrogram ##### 
+    cut_t, map_list = phylo.cut_tree(t, k)
+    m_df = pd.DataFrame(map_list)
+    m_df.columns = [cnv_index_name, 'hcluster']
+    m_df.index = m_df[cnv_index_name]
+    del m_df[cnv_index_name] 
+
+    ##### build nested tree ##### 
+    res = phylo.get_nested_tree_json(t, cut_n)
+    json_fn = out_prefix + '_cut{}.json'.format(cut_n)
+    with open(json_fn, 'w') as f:
+        f.write(res)
+
+    ##### append meta info #####
+    if meta_fn:
+        meta_df = io.read_meta_fn(meta_fn, cnv_index_name)
+        meta_df = pd.merge(meta_df, m_df, how='outer', on=cnv_index_name)
+    else:
+        meta_df = m_df
+    '''
+    # PCA
+    df = embedding.get_pca(cnv_df, cell_names, cnv_index_name)
+    meta_df = pd.merge(meta_df, df, how='outer', on=cnv_index_name)
+    # TSNE 
+    df = embedding.get_tsne(cnv_df, cell_names, cnv_index_name)
+    meta_df = pd.merge(meta_df, df, how='outer', on=cnv_index_name)
+    # UMAP 
+    df = embedding.get_umap(cnv_df, cell_names, cnv_index_name)
+    meta_df = pd.merge(meta_df, df, how='outer', on=cnv_index_name)
+    meta_fn = out_prefix + '_meta_scvar.csv' 
+    meta_df.to_csv(meta_fn)
+    '''
+
+    ##### build tree by meta category label #####
+    evo_dict = {}
+    for col in meta_df.columns:
+        if col.startswith('e_') or col.startswith('c_'):
+            continue
+        df = pd.merge(meta_df[col], cnv_df, how='outer', on=cnv_index_name)
+        df = df.groupby(col).mean()
+        newick = phylo.build_hc_tree(df, col)
+        t = phylo.get_tree_from_newick(newick)
+        evo_dict[col] = phylo.get_evo_tree_dict(t, meta_df[col]) 
+    res = json.dumps(evo_dict, indent=4)
+    json_fn = out_prefix + '_evo{}.json'.format(k)
+    with open(json_fn, 'w') as f:
+        f.write(res)
+
+def run(cnv=None, **args):
+    if cnv:
+        run_cnv(**args)
 
 
 if __name__ == "__main__":
--- script/utils/phylogenetic.py
+++ script/utils/phylogenetic.py
@@ -38,22 +38,22 @@ def get_newick(Z, cell_names):
     return netwick
 
 
-def get_tree(x, index, index_name, k=3):
+def build_hc_tree(df, index_name):
     '''
     x = np.array([662., 877., 255., 412., 996., 295., 468., 268.,
                   400., 754., 564., 138., 219., 869., 669.])
     index = range(6)
     '''
-    Z = hierarchy.linkage(x)
+    Z = hierarchy.linkage(df)
 
     # get newick 
-    newick = get_newick(Z, index) 
-
+    newick = get_newick(Z, df.index) 
+    '''
     # get hc
-    df = pd.DataFrame(hierarchy.cut_tree(Z, 7)[:,0]+1, columns=['hcluster'], index=index)
-    df.index.name = index_name
-
-    return newick, df
+    m_df = pd.DataFrame(hierarchy.cut_tree(Z, k)[:,0]+1, columns=['hcluster'], index=df.index)
+    m_df.index.name = index_name
+    '''
+    return newick
 
 
 def get_nested_tree_json(t, k):
@@ -62,14 +62,13 @@ def get_nested_tree_json(t, k):
 
 
 def get_nested_tree_aux(t, k):
-    cut_t = cut_tree(t, k)
-    leafs = [n.name for n in t.nodes if not n.children]
+    cut_t, _ = cut_tree(t, k)
     node_dict = {}
     res = {}
     res['dist_to_root'] = t.dist_to_root
     res['parent'] = t.parent.name if t.parent else 'NONE' 
     res['newick'] = cut_t.write(format=1) 
-    res['leafs'] = leafs
+    res['leafs'] = [n.name for n in t.leafs]
     node_dict[t.name] = res
     if t.children:
         for c in t.children:
@@ -78,21 +77,28 @@ def get_nested_tree_aux(t, k):
 
     return node_dict 
 
-
-
-def node_to_dict(t):
+def get_evo_tree_dict(t, df):
+    node_list = sorted(t.nodes, key=lambda n: n.dist_to_root)
     res = {}
+    res['name'] = t.name
+    res['parent'] = t.parent.name if t.parent else 'NONE' 
+    res['newick'] = t.write(format=1) 
+    res['lifetime'] = node_list[-1].dist_to_root/2*3
+    res['dist_to_root'] = t.dist_to_root
+    res['num_cells'] = len(df)
+    res['leafs'] = [n.name for n in t.leafs]
+    res['links'] = t.links
+    nodes_dict = {}
+
+    set_tree_coords(t, df)
+    node_list = sorted(t.nodes, key=lambda n: n.dist_to_root)
+    for n in node_list:
+        c = n.closest_child.name if n.closest_child else 'NONE'
+        nodes_dict[n.name] = [n.x, n.y, n.start_y, n.end_y, c] 
+    res['node_list'] = list(nodes_dict.keys())
+    res['nodes'] = nodes_dict
     return res
-
-
-def get_leafs(t):
-    if not t.children:
-        return [t.name]
-    res = []
-    for c in t.children:
-        res += get_leafs(c)
-    return res
-
+    
 
 def cut_tree(t, k, prefix='n'):
     t = copy.deepcopy(t)
@@ -123,20 +129,26 @@ def cut_tree(t, k, prefix='n'):
         print([n.name for n in nodes])
         print([n.name for n in leafs])
         '''
-    cut_tree_aux(t, leafs)
+    map_list = cut_tree_aux(t, { n: i+1 for i, n in enumerate(leafs)})
     set_tree(t, prefix=prefix)
-    return t
+    return t, map_list
 
 
 def cut_tree_aux(t, leafs):
     if not t.children:
-        return
+        if t in leafs.keys():
+            return [(t.name, leafs[t])]
+        else:
+            return [] 
 
+    map_list = []
     for c in t.children:
-        if c in leafs:
+        if c in leafs.keys():
+            map_list += [(n.name ,leafs[c]) for n in c.leafs]
             c.children = []
         else:
-            cut_tree_aux(c, leafs)
+            map_list += cut_tree_aux(c, leafs)
+    return map_list 
 
 
 def get_tree_from_newick(newick):
@@ -151,25 +163,56 @@ def set_tree(t, node_id=0, prefix='n'):
         if prefix == 'c':
             t.name = '{}{}'.format(prefix, node_id)
         t.dist_to_root = t.dist
+        t.lifetime = t.dist
         t.leafs = [t]
         t.nodes = [t]
+        t.links = []
+        t.closest_child = None
         return
 
     t.name = '{}{}'.format(prefix, node_id)
     t.dist_to_root = t.dist
     t.leafs = []
+    t.links = []
     t.nodes = [t]
     current_node_id = node_id + 1
+    t.closest_child = t.children[0]
     for c in t.children:
+        if c.dist < t.closest_child.dist:
+            t.closest_child = c
         c.parent = t
         set_tree(c, current_node_id, prefix=prefix)
+        t.links += [(t.name, c.name)]
+        t.links += c.links
         for n in c.nodes:
             n.dist_to_root += t.dist 
             if n.children or prefix == 'c':
                 current_node_id += 1 
         t.nodes += c.nodes 
         t.leafs += c.leafs
-    
+   
+def set_tree_coords(t, df):
+    map_dict = df.apply(str).value_counts().to_dict()
+    count = 0 
+    for i, n in enumerate(t.leafs):
+        n.start_y = count 
+        count += map_dict[n.name]
+        n.end_y = count 
+        n.y = i + 0.5 
 
+    set_tree_coords_aux(t)
+
+
+def set_tree_coords_aux(t):
+    t.x = t.dist_to_root
+    if not t.children:
+        return 
+    for c in t.children:
+        set_tree_coords_aux(c)
+    t.y = (t.children[0].y + t.children[-1].y)/2
+    t.start_y = t.children[0].start_y
+    t.end_y = t.children[-1].end_y
+    return 
+        
 
 
