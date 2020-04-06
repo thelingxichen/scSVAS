cbf7144b5ab0f35d6ca1df6f3eebbc7e7f116345
chenlingxi@ <chanlingxi@gmail.com>
2020-04-06 16:49:23 +0800

    add io read 10x bed fn

--- script/utils/io.py
+++ script/utils/io.py
@@ -1,4 +1,4 @@
-
+import csv
 import pandas as pd
 
 
@@ -22,3 +22,14 @@ def read_target_gene(target_gene_fn):
     return genes
 
 
+def read_10x_bed_fn(bed_fn, confidence=15):
+    bed_file = filter(lambda row: row.startswith('#chrom') or not row.startswith('#'), open(bed_fn, 'r'))
+    for i, row in enumerate(csv.DictReader(bed_file, delimiter='\t')):
+        if int(row.get('event_confidence', confidence)) < confidence:
+            continue
+        if row['copy_number'] == 'NA':
+            continue
+        chrom, start, end, cell_id, cn = row['#chrom'], row['start'], row['end'], row['id'], row['copy_number']
+        if not chrom.startswith('chr'):
+            chrom = 'chr' + chrom
+        yield chrom, int(start), int(end), cell_id, int(cn)
