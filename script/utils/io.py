7e151f6532e09500ac5622592c403a72b9936288
chenlingxi@dl380a <chanlingxi@gmail.com>
2020-06-23 13:37:24 +0800

    update io

--- script/utils/io.py
+++ script/utils/io.py
@@ -6,19 +6,19 @@ def read_cnv_fn(cnv_fn, index_name):
     if type(cnv_fn) != str:
         return cnv_fn
     df = pd.read_csv(cnv_fn, index_col=0)
-    df = df.fillna(2) 
-    df.index.name = index_name 
+    df = df.fillna(2)
+    df.index.name = index_name
     return df
 
 
-def read_meta_fn(meta_fn, index_name):
+def read_meta_fn(meta_fn, index_name='cell_id'):
     df = pd.read_csv(meta_fn, index_col=index_name)
     return df
 
 
 def read_target_gene(target_gene_fn):
     genes = []
-    for line in open(target_gene_bed_fn, 'r'):
+    for line in open(target_gene_fn, 'r'):
         genes.append(line.strip().split('\t')[3])
     genes.sort()
     return genes
