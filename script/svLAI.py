d4a72ecc54eea354698c6003302cbcb3d43ab33c
chenlingxi@ <chanlingxi@gmail.com>
2020-07-20 16:22:18 +0800

    update scLAI

--- script/svLAI.py
+++ script/svLAI.py
@@ -16,21 +16,14 @@ Options:
     --ref=STR                   Reference version, [default: hg38]
 """
 import docopt
-import numpy as np
-import json
 import pandas as pd
 import collections
 
-from biotool import genome
-from biotool import gene 
+from biotool import gene
 
-from utils import phylogenetic as phylo
-from utils import io
-from utils import annotation as anno
 import spacelineage
 
-from SVAS.scripts import sv
-from SVAS.scripts.compsv import complexsv 
+from SVAS.scripts.compsv import complexsv
 
 
 def get_barcode_clone(group_meta_fn, barcode_group_fn):
@@ -38,7 +31,7 @@ def get_barcode_clone(group_meta_fn, barcode_group_fn):
     barcode_group_df = pd.read_csv(barcode_group_fn)
     df = pd.merge(barcode_group_df, group_meta_df, on='group').set_index('barcode')
     barcode2clone = df['label'].to_dict()
-    clone2barcodes = {} 
+    clone2barcodes = {}
     for bc, c in barcode2clone.items():
         if c in clone2barcodes:
             clone2barcodes[c].append(bc)
@@ -47,7 +40,7 @@ def get_barcode_clone(group_meta_fn, barcode_group_fn):
     return barcode2clone, clone2barcodes
 
 
-def run_call(tree_fn=None, group_meta_fn=None, barcode_group_fn=None, out_prefix=None, 
+def run_call(tree_fn=None, group_meta_fn=None, barcode_group_fn=None, out_prefix=None,
              mut_fn=None, sv_fn=None, sample=None,
              ref='hg38', target_gene_fn=None, **args):
     barcode2clone, clone2barcodes = get_barcode_clone(group_meta_fn, barcode_group_fn)
@@ -59,16 +52,20 @@ def run_call(tree_fn=None, group_meta_fn=None, barcode_group_fn=None, out_prefix
         sv_df_list.append(pd.read_csv(f, sep='\t').set_index('AnnotSV ID'))
     sv_df = pd.concat(sv_df_list)
     sv_df = sv_df[sv_df['Gene name'].isin(gene_list)]
+    '''
     sv_df.INFO = sv_df.INFO.apply(lambda x: x.split(';')[0])
     sv_df = sv_df[sv_df.INFO.apply(lambda x: x.startswith('BX='))]
     sv_df.INFO = sv_df.INFO.apply(lambda x: [barcode2clone.get(bc.replace('_1', ''), '') for bc in x.replace('BX=', '').split(',')])
     sv_df.INFO = sv_df.INFO.apply(lambda cs: collections.Counter(c for c in cs if c))
     sv_df.INFO = sv_df.INFO.apply(lambda cs: [(c, n) for c, n in cs.items() if n >= 10])
+    print(sv_df.INFO)
     sv_df = sv_df[sv_df.INFO.apply(lambda x: len(x) != 0)]
     info = sv_df.INFO.apply(lambda x: ','.join(sorted([a for a, b in x])))
     print(collections.Counter(info.to_list()))
     sv_df[['Gene name', 'INFO']].to_csv('test.csv')
-    sv_df.ID = sv_df.ID.apply(lambda x: str(x).replace(':1', '').replace(':2', '')) 
+    '''
+
+    sv_df.ID = sv_df.ID.apply(lambda x: str(x).replace(':1', '').replace(':2', ''))
 
     id2genes = {}
     for p in (list(zip(sv_df.ID, sv_df['Gene name']))):
@@ -77,7 +74,7 @@ def run_call(tree_fn=None, group_meta_fn=None, barcode_group_fn=None, out_prefix
             id2genes[id].add(g)
         else:
             id2genes[id] = set([g])
-    complexsv.run_call(sv_fn=sv_fn, sample=sample, out_dir='./test', id2genes=id2genes)
+    complexsv.run_call(sv_fn=sv_fn, sample=sample, out_dir=out_prefix, id2genes=id2genes)
 
 
 def run(call=None, **args):
